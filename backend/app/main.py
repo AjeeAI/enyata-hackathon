0a1a0c5cd6174a910118ba8919d8e2245544472d
Adesoji Ajijolaoluwa <ajeeaidev@gmail.com>
2026-03-28 00:00:03 +0100

    Submission ready

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="EduIntellect API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://eduintellect-three.vercel.app"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
